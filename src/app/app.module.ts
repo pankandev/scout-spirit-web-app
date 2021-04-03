fb7928277b18479bb975dd0f8d68d9a5a7a8b9d0
Paths <pcampaa@gmail.com>
2021-04-03 13:18:29 -0300

    add amplify authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {AmplifyUIAngularModule} from '@aws-amplify/ui-angular';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    AmplifyUIAngularModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/aws-exports.ts
@@ -0,0 +1,11 @@
+export default {
+  UserAgent: 'aws-amplify-cli/2.0',
+  Version: '1.0',
+  Auth: {
+    identityPoolId: 'us-west-2:b6535368-12b8-45a3-9308-1c30cec20964',
+    region: 'us-west-2',
+    userPoolId: 'us-west-2_Tau9d2K6B',
+    userPoolWebClientId: '1i9t2btpfetg45a8d1cg3r5qgs',
+    mandatorySignIn: true,
+  },
+};
