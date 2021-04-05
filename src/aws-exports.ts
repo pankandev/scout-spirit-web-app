e05c89a109450dcc1e29b84469da2dcaba46baef
Paths <pcampaa@gmail.com>
2021-04-05 07:58:33 -0400

    update identity pool ID

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -4,7 +4,7 @@ export default {
   UserAgent: 'aws-amplify-cli/2.0',
   Version: '1.0',
   Auth: {
-    identityPoolId: 'us-west-2:b6535368-12b8-45a3-9308-1c30cec20964',
+    identityPoolId: 'us-west-2:160f7faf-21f1-4611-9f73-bcb1849fe4be',
     region: 'us-west-2',
     userPoolId: 'us-west-2_SwOlCeGN1',
     userPoolWebClientId: '12np1gn5jf4dcgfi6ub47e6oeq',
