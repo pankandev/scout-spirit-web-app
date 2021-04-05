87fc265bf0dcc922d9b64163ec9ce41482b6c5ad
Paths <pcampaa@gmail.com>
2021-04-05 07:45:49 -0400

    update sign in/sign up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'scout-personal-progression-webapp';
+
+  formFields = [
+    {
+      type: 'email',
+      label: 'E-mail',
+      placeholder: 'mail@mail.com',
+      required: true,
+    },
+    {
+      type: 'name',
+      label: 'Nombre',
+      placeholder: 'Juan',
+      required: true,
+    },
+    {
+      type: 'middle_name',
+      label: 'Segundo Nombre',
+      placeholder: '...',
+      required: false,
+    },
+    {
+      type: 'family_name',
+      label: 'Apellido',
+      placeholder: 'Pérez',
+      required: true,
+    },
+    {
+      type: 'password',
+      label: 'Contraseña',
+      placeholder: 'Contraseña',
+      required: true,
+    },
+  ];
 }
--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -6,15 +6,15 @@ export default {
   Auth: {
     identityPoolId: 'us-west-2:b6535368-12b8-45a3-9308-1c30cec20964',
     region: 'us-west-2',
-    userPoolId: 'us-west-2_Tau9d2K6B',
-    userPoolWebClientId: '1i9t2btpfetg45a8d1cg3r5qgs',
+    userPoolId: 'us-west-2_SwOlCeGN1',
+    userPoolWebClientId: '12np1gn5jf4dcgfi6ub47e6oeq',
     mandatorySignIn: true,
   },
   API: {
     endpoints: [
       {
         name: 'PPSAPI',
-        endpoint: 'https://5ls6ka1vg1.execute-api.us-west-2.amazonaws.com/Prod',
+        endpoint: 'https://j2cpy7mcrh.execute-api.us-west-2.amazonaws.com/Prod',
         custom_header: async () => {
           const token = (await Auth.currentSession()).getIdToken().getJwtToken();
           return {
